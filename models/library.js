62f00e5f6c4206097496391b2d17b818840130ea
Kalyan1003 <143153921+Kalyan1003@users.noreply.github.com>
2023-11-29 17:28:53 -0600

    Login page has been created and added a validator and buttons for each collection

--- models/library.js
+++ models/library.js
@@ -1,8 +1,19 @@
 const mongoose = require("mongoose")
 const librarySchema = mongoose.Schema({
-name: String,
-timings: Number,
-place: String
+name:{
+    type:String,
+    minlength:1,
+    maxlength:15,
+},
+timings: {
+    type:Number,
+    min:0,
+    max:12,
+},
+place: {
+    type:String,
+
+},
 })
 module.exports = mongoose.model("library", librarySchema)
 
--- routes/Library.js
+++ routes/Library.js
@@ -1,6 +1,15 @@
 var express = require('express');
 const library_controlers= require('../controllers/library');
 var router = express.Router();
+// A little function to check if we have an authorized user and continue on
+// redirect to login.
+const secured = (req, res, next) => {
+if (req.user){
+return next();
+}
+res.redirect("/login");
+}
+
 /* GET costumes */
 router.get('/', library_controlers.library_view_all_Page );
 module.exports = router;
@@ -9,12 +18,12 @@ module.exports = router;
 router.get('/detail', library_controlers.library_view_one_Page);
 
 /* GET create costume page */
-router.get('/create', library_controlers.library_create_Page);
+router.get('/create',secured, library_controlers.library_create_Page);
 
 /* GET create update page */
-router.get('/update', library_controlers.library_update_Page);
+router.get('/update',secured, library_controlers.library_update_Page);
 
 /* GET delete costume page */
-router.get('/delete', library_controlers.library_delete_Page);
+router.get('/delete', secured,library_controlers.library_delete_Page);
 
 
